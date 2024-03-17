9d8b88d992089feee10a33403c5f698d8be40959
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-03-17 16:55:55 +0530

    bfs

---
+++ bfs.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// breadth first search
+vector<int> bfs(int n, vector<int> adj[]){
+    int vis[n]={0};
+    vis[0]=1;
+    queue<int> q;
+    q.push(0);
+    vector<int> bfs;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                vis[it]=1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
+}
+
+void addEdge(vector <int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printAns(vector <int> &ans) {
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+}
+
+int main(){
+    vector <int> adj[6];
+    
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 0, 4);
+
+    vector <int> ans = bfs(5, adj);
+    printAns(ans);
+    return 0;
+}
